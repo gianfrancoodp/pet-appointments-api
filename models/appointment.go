bdda07f94de7f76f066e8687ba897e97adac3897
Gian Ortega <gianfrancoodp@gmail.com>
2023-05-08 20:13:58 -0300

    The owner model, routes and controller have been added to the project.

--- models/appointment.go
+++ models/appointment.go
@@ -9,7 +9,7 @@ type Appointment struct {
 	Id          primitive.ObjectID `json:"id,omitempty"`
 	OwnerId     int                `json:"ownerId,omitempty" validate:"required"`
 	PetId       int                `json:"petId,omitempty" validate:"required"`
-	PartnerId   int                `json:"PartnerId,omitempty" validate:"required"`
+	PartnerId   int                `json:"partnerId,omitempty" validate:"required"`
 	Service     string             `json:"service,omitempty" validate:"required"`
 	Amount      float64            `json:"amount,omitempty" validate:"required"`
 	PaymentType string             `json:"paymentType,omitempty" validate:"required"`
